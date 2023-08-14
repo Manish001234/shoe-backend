e0b3b36b490431e1bb603693a8286bfda7fd4809
Manish Reddy <89566773+Manish001234@users.noreply.github.com>
2023-08-14 11:02:19 +0530

    cart controleer added

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ const cors = require("cors");
 
 const app = express();
 // require("dotenv").config()
-const port = process.env.PORT || 2345;
+const port = 2345;
 
 app.use(express.json());
 app.use(express.static("public"));
 app.use(cors());
 
 const ShoeController = require("./controllers/shoe.controller");
+const CartMobileController = require("./controllers/cart.controller")
 
 app.use("/Shoes", ShoeController);
+app.use("/mobilecart",CartMobileController)
 
 app.listen(port, async (req, res) => {
   try {
